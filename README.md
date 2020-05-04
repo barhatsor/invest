2359643ba4d9c61d8a8c83bd8c4d3eff645dfa1f
Some Body <somebody@someoneinc.com>
2020-05-04 19:23:26 +0300

    Updated JS

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # invest
 A webapp for investing in the stock market.
-Includes ***stock filtering***, ***latest news***, ***realtime stock updates*** and ***a sleek UI***.
+Always live at [investor.netlify.app](https://investor.netlify.app/).
+
+## Todo
+######☑️ `GET` ~~data from API and integrate into HTML~~
+######☑️ ~~Sleek UI~~
+#####☐ Stock filtering
+#####⬜ Updates in realtime
+#####⬜ Latest news
+
+## Pushing
+You're awesome. Clone the repository to your computer and run `index.html`.
+
+## License
+MIT
+
+---
+Uses the IEX Cloud API.
--- js/main.js
+++ js/main.js
@@ -1,59 +1,124 @@
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var obj = JSON.parse(this.responseText);
-        var out = "";
-        var precent;
-        for (var prop in obj) {
-            if (obj[prop].quote.changePercent < 0) {
-                precent = "<h2 class='red'>";
-            } else {
-                precent = "<h2>+";
-            }
-            out +=
-                "<div class='entry'><h1>" +
-                obj[prop].quote.symbol +
-                "</h1><p>" +
-                obj[prop].quote.companyName +
-                "</p>" +
-                precent +
-                ((Math.round(obj[prop].quote.changePercent * 100) / 100).toFixed(1) * 1
-                ).toString() +
-                "%</h2><span>" +
-                ((Math.round(obj[prop].quote.latestPrice * 100) / 100).toFixed(1) * 1
-                ).toString() +
-                "</span></div>";
-        }
-        document.querySelector(".entries").innerHTML =
-            out + "<a href='https://iexcloud.io'>Data provided by IEX Cloud</a>";
-        document.querySelector(".title").style.animation =
-            "title .2s ease forwards";
-        document.querySelectorAll(".entry").forEach(i => {
-            if (scrldIntoView(i)) {
-                i.style.animation = "entry .5s .2s ease forwards";
-            }
-        });
+/* Main thread */
+
+// Initiate API handler
+APIhandler = new APIHandler();
+
+APIhandler.sendAPIReq();
+APIhandler.handleResponse();
+
+
+/* Handle everything API */
+class APIHandler {
+
+    constructor(requestedStocks)
+    {
+        this.APIurl = "https://cloud.iexapis.com/stable/stock/market/batch?symbols=aapl,mcd,amzn,cost,lmt,fb,msft,ba,wmt,t&types=quote&displayPercent=true&token=pk_370633a589a240f29304a7420b9960ec";
+        // Dividends get a special URL
+        this.APIurl_div = "https://cloud.iexapis.com/stable/stock/AAPL/dividends/5y?token=pk_370633a589a240f29304a7420b9960ec";
     }
-};
 
-xmlhttp.open(
-    "GET",
-    "https://cloud.iexapis.com/stable/stock/market/batch?symbols=aapl,mcd,amzn,cost,lmt,fb,msft,ba,wmt,t&types=quote&displayPercent=true&token=pk_370633a589a240f29304a7420b9960ec",
-    true
-);
-xmlhttp.send();
+    // Send API request
+    sendRequest(div) {
+        this.xmlhttp = new XMLHttpRequest();
+        // Requested dividends or not
+        if (!div) {
+            this.xmlhttp.open("GET", this.APIurl, true);
+            this.xmlhttp.send();
+        } else {
+            this.xmlhttp.open("GET", this.APIurl_div, true);
+            this.xmlhttp.send();
+        }
+    }
 
-document.querySelector(".filter-button").addEventListener("click", e => {
+    // Filter API response
+    filterResponse(response)
+    {
+        let result = response.filter(peRatio => peRatio < 11);
+        return result;
+    }
+
+    // Handle API response
+    handleResponse() {
+        this.APIresponse = null;
+        this.xmlhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                // Parse response from JSON to array
+                var APIresponse = JSON.parse(this.responseText);
+                // Filter response
+                filteredResponse = this.filterResponse(APIresponse);
+                // Finally, build the HTML
+                buildHTML(filteredResponse);
+            }
+        }
+    }
+}
+
+
+/* Handle everything HTML */
+buildHTML(response) {
+    // Finished HTML goes here
+    this.out = "";
+    // Create precent var to track negative precentages
+    var precent;
+    // For each stock in API response
+    for (var prop in response) {
+        // If precent is negative, color it red
+        if (response[prop].quote.changePercent < 0) {
+            precent = "<h2 class='red'>";
+        }
+        // Else, default to green
+        else {
+            precent = "<h2>+";
+        }
+        // Build stock entries
+        this.out +=
+            "<div class='entry'><h1>" +
+            response[prop].quote.symbol +
+            "</h1><p>" +
+            response[prop].quote.companyName +
+            "</p>" +
+            precent +
+            // Round precentages to 2 digits & remove trailing zeros
+            ((Math.round(response[prop].quote.changePercent * 100) / 100).toFixed(1) * 1).toString() +
+            "%</h2><span>" +
+            // Do the same for stock prices
+            ((Math.round(response[prop].quote.latestPrice * 100) / 100).toFixed(1) * 1).toString() +
+            "</span></div>";
+    }
+
+    // Inject the finished HTML into the page
+    document.querySelector(".entries").innerHTML =
+        View.out + "<a href='https://iexcloud.io'>Data provided by IEX Cloud</a>";
+
+    // And remove the loading screen
+    document.querySelector(".title").style.animation =
+        "title .2s ease forwards";
+}
+
+
+/* --UI-related JS-- */
+
+// Toggle filter button
+document.querySelector(".filter-button").addEventListener("click", (e) => {
     document.querySelector(".filter-button").classList.toggle("active");
     document.querySelector(".filters-wrapper").classList.toggle("active");
 });
 
-document.querySelectorAll(".filter").forEach(filter => {
-    filter.addEventListener("click", e => {
+// Toggle each filter
+document.querySelectorAll(".filter").forEach((filter) => {
+    filter.addEventListener("click", (e) => {
         filter.classList.toggle("active");
     });
 });
 
+// Entries animate when scrolled into view
+window.onscroll = function (e) {
+    document.querySelectorAll(".entry").forEach((entry) => {
+        if (scrldIntoView(entry)) {
+            entry.style.animation = "entry .5s ease forwards";
+        }
+    })
+}
 function scrldIntoView(el) {
     var rect = el.getBoundingClientRect();
     var elemTop = rect.top;
@@ -61,10 +126,20 @@ function scrldIntoView(el) {
     return isVisible;
 }
 
-window.onscroll = function(e) {
-    document.querySelectorAll(".entry").forEach(entry => {
-        if (scrldIntoView(entry)) {
-            entry.style.animation = "entry .5s ease forwards";
-        }
-    });
-};
+
+/* Unimplemented (yet!) filter code */
+
+/*
+var filter = peRatio.filter(function(ratio) {
+  return ratio < 15;
+});
+console.log(filter);
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    var obj = JSON.parse(div);
+    try {console.log(obj[0].amount);} catch {};
+  }};
+xmlhttp.open("GET", "https://cloud.iexapis.com/stable/stock/AAPL/dividends/5y?token=pk_370633a589a240f29304a7420b9960ec", true);
+xmlhttp.send(); */
