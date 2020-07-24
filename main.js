4e2150670fb0e3a4f51d473afb18d1fd588a5cfe
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2020-07-24 17:04:21 +0300

    Update main.js

--- main.js
+++ main.js
@@ -218,6 +218,46 @@ function renderSuggestions(resp) {
 }
 
 
+/* Swipe to remove stocks */
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||
+         evt.originalEvent.touches;
+}                                                     
+
+function handleTouchStart(evt) {
+    var currentElement = evt.target;
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {
+        if ( xDiff < 0 ) {
+            currentElement.style.left = xDiff;
+        }
+    }
+    xDown = null;
+    yDown = null;                                             
+};
+
+
 /* Portfolio local storage */
 var tArray = [];
 var tickers;
