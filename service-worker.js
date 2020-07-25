59719ad0fe318ddc7284b8e54c47cb55ad684d84
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2020-07-25 15:29:18 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -9,11 +9,13 @@ const FILES_TO_CACHE = [
 ];
 
 self.addEventListener('install', (evt) => {
-  console.log('[ServiceWorker] Install');
+  //console.log('[ServiceWorker] Install');
+  
   // Precache static resources
   evt.waitUntil(
       caches.open(CACHE_NAME).then((cache) => {
-        console.log('[ServiceWorker] Pre-caching offline page');
+        //console.log('[ServiceWorker] Pre-caching offline page');
+        
         return cache.addAll(FILES_TO_CACHE);
       })
   );
@@ -21,13 +23,15 @@ self.addEventListener('install', (evt) => {
 });
 
 self.addEventListener('activate', (evt) => {
-  console.log('[ServiceWorker] Activate');
+  //console.log('[ServiceWorker] Activate');
+  
   // Remove previous cached data from disk.
   evt.waitUntil(
       caches.keys().then((keyList) => {
         return Promise.all(keyList.map((key) => {
           if (key !== CACHE_NAME) {
-            console.log('[ServiceWorker] Removing old cache', key);
+            //console.log('[ServiceWorker] Removing old cache', key);
+            
             return caches.delete(key);
           }
         }));
@@ -37,7 +41,8 @@ self.addEventListener('activate', (evt) => {
 });
 
 self.addEventListener('fetch', (evt) => {
-  console.log('[ServiceWorker] Fetch', evt.request.url);
+  //console.log('[ServiceWorker] Fetch', evt.request.url);
+  
   // Fetch event handler
   if (evt.request.mode !== 'navigate') {
     // Not a page navigation, bail
