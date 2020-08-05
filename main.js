66682730f07d392cd4bbee465c9173b3b4b13f73
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2020-08-05 22:17:08 +0300

    Update main.js

--- main.js
+++ main.js
@@ -327,7 +327,7 @@ function switchTab(url) {
 // Round
 function round(value, precision) {
     var multiplier = Math.pow(10, precision || 0);
-    return (Math.round(value * multiplier) / multiplier < 0) ? Math.round(value * multiplier) / multiplier : '-';
+    return ((Math.round(value * multiplier) / multiplier != 0) ? Math.round(value * multiplier) / multiplier : '-');
 }
 
 // HTTP Request
