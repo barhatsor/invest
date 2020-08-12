3610416372cba3fbc07da95ec0060a9334888052
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2020-08-12 11:53:39 +0300

    Update main.js

--- main.js
+++ main.js
@@ -86,6 +86,11 @@ class stockEntries {
         
         // Inject the finished HTML into the page
         document.querySelector(".entries").innerHTML = this.out;
+       
+        // Add swipe eventListener for stocks
+        document.querySelectorAll(".entry").forEach(entry => {
+           addSwipeListener(entry);
+        }
     }
 
     // Toggle stock details
@@ -207,48 +212,60 @@ function renderSuggestions(resp) {
 
 
 /* Swipe to remove stocks */
-document.addEventListener('touchstart', handleTouchStart, false);        
-document.addEventListener('touchmove', handleTouchMove, false);
+function swipedetect(el, callback){
 
-var currentElement = null;
+    var touchsurface = el,
+    swipedir,
+    startX,
+    startY,
+    distX,
+    distY,
+    threshold = 150, //required min distance traveled to be considered swipe
+    restraint = 100, // maximum distance allowed at the same time in perpendicular direction
+    allowedTime = 300, // maximum time allowed to travel that distance
+    elapsedTime,
+    startTime,
+    handleswipe = callback || function(swipedir){}
 
-var xDown = null;                                                        
-var yDown = null;
+    touchsurface.addEventListener('touchstart', function(e){
+        var touchobj = e.changedTouches[0]
+        swipedir = 'none'
+        dist = 0
+        startX = touchobj.pageX
+        startY = touchobj.pageY
+        startTime = new Date().getTime() // record time when finger first makes contact with surface
+        e.preventDefault()
+    }, false)
 
-function getTouches(evt) {
-  return evt.touches ||
-         evt.originalEvent.touches;
-}                                                     
+    touchsurface.addEventListener('touchmove', function(e){
+        e.preventDefault() // prevent scrolling when inside DIV
+    }, false)
 
-function handleTouchStart(evt) {
-    currentElement = evt.target;
-    const firstTouch = getTouches(evt)[0];                                      
-    xDown = firstTouch.clientX;                                      
-    yDown = firstTouch.clientY;                                      
-};                                                
-
-function handleTouchMove(evt) {
-    if ( ! xDown || ! yDown ) {
-        return;
-    }
-
-    var xUp = evt.touches[0].clientX;                                    
-    var yUp = evt.touches[0].clientY;
-
-    var xDiff = xDown - xUp;
-    var yDiff = yDown - yUp;
-
-    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {
-        if ( xDiff > 0 ) {
-            console.log(currentElement);
-            console.log(xDiff);
-            currentElement.style.left = xDiff;
+    touchsurface.addEventListener('touchend', function(e){
+        var touchobj = e.changedTouches[0]
+        distX = touchobj.pageX - startX // get horizontal dist traveled by finger while in contact with surface
+        distY = touchobj.pageY - startY // get vertical dist traveled by finger while in contact with surface
+        elapsedTime = new Date().getTime() - startTime // get time elapsed
+        if (elapsedTime <= allowedTime){ // first condition for awipe met
+            if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ // 2nd condition for horizontal swipe met
+                swipedir = (distX < 0)? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
+            }
+            else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){ // 2nd condition for vertical swipe met
+                swipedir = (distY < 0)? 'up' : 'down' // if dist traveled is negative, it indicates up swipe
+            }
         }
-    }
-    xDown = null;
-    yDown = null;                                             
-};
+        handleswipe(swipedir)
+        e.preventDefault()
+    }, false)
+}
 
+function addSwipeListener(el) {
+   swipedetect(el, function(swipedir) {
+       swipedir contains either "none", "left", "right", "top", or "down"
+       if (swipedir =='left')
+           alert('You just swiped left!');
+   })
+}
 
 /* Portfolio local storage */
 var tArray = [];
