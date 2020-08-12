f36cb776ba74aaf60bf3199c3b8e21ceeb1895ac
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2020-08-12 12:10:06 +0300

    Update main.js

--- main.js
+++ main.js
@@ -213,61 +213,43 @@ function renderSuggestions(resp) {
 
 
 /* Swipe to remove stocks */
-function swipedetect(el, callback){
+function addSwipeListener(gesuredZone) {
+   var touchstartX = 0;
+   var touchstartY = 0;
+   var touchendX = 0;
+   var touchendY = 0;
 
-    var touchsurface = el,
-    swipedir,
-    startX,
-    startY,
-    distX,
-    distY,
-    threshold = 150, //required min distance traveled to be considered swipe
-    restraint = 100, // maximum distance allowed at the same time in perpendicular direction
-    allowedTime = 300, // maximum time allowed to travel that distance
-    elapsedTime,
-    startTime,
-    handleswipe = callback || function(swipedir){}
+   gesuredZone.addEventListener('touchstart', function(event) {
+       touchstartX = event.screenX;
+       touchstartY = event.screenY;
+   }, false);
 
-    touchsurface.addEventListener('touchstart', function(e){
-        var touchobj = e.changedTouches[0]
-        swipedir = 'none'
-        dist = 0
-        startX = touchobj.pageX
-        startY = touchobj.pageY
-        startTime = new Date().getTime() // record time when finger first makes contact with surface
-        e.preventDefault()
-    }, false)
+   gesuredZone.addEventListener('touchend', function(event) {
+       touchendX = event.screenX;
+       touchendY = event.screenY;
+       handleGesure();
+   }, false); 
 
-    touchsurface.addEventListener('touchmove', function(e){
-        e.preventDefault() // prevent scrolling when inside DIV
-    }, false)
-
-    touchsurface.addEventListener('touchend', function(e){
-        var touchobj = e.changedTouches[0]
-        distX = touchobj.pageX - startX // get horizontal dist traveled by finger while in contact with surface
-        distY = touchobj.pageY - startY // get vertical dist traveled by finger while in contact with surface
-        elapsedTime = new Date().getTime() - startTime // get time elapsed
-        if (elapsedTime <= allowedTime){ // first condition for awipe met
-            if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ // 2nd condition for horizontal swipe met
-                swipedir = (distX < 0)? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
-            }
-            else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){ // 2nd condition for vertical swipe met
-                swipedir = (distY < 0)? 'up' : 'down' // if dist traveled is negative, it indicates up swipe
-            }
-        }
-        handleswipe(swipedir)
-        e.preventDefault()
-    }, false)
+   function handleGesure() {
+       var swiped = 'swiped: ';
+       if (touchendX < touchstartX) {
+           console.log(swiped + 'left!');
+       }
+       if (touchendX > touchstartX) {
+           console.log(swiped + 'right!');
+       }
+       if (touchendY < touchstartY) {
+           console.log(swiped + 'down!');
+       }
+       if (touchendY > touchstartY) {
+           console.log(swiped + 'left!');
+       }
+       if (touchendY == touchstartY) {
+           console.log('tap!');
+       }
+   }
 }
 
-function addSwipeListener(el) {
-   console.log(el);
-   swipedetect(el, function(swipedir){
-       swipedir contains either "none", "left", "right", "top", or "down";
-       if (swipedir =='left')
-           alert('You just swiped left!');
-   })
-}
 
 /* Portfolio local storage */
 var tArray = [];
