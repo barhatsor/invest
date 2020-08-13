d7a05118ff41558a582f944ebe03cca793dd3576
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2020-08-13 12:18:54 +0300

    Update main.js

--- main.js
+++ main.js
@@ -381,11 +381,11 @@ document.querySelectorAll(".filter").forEach((filter) => {
         // If the filter's active
         if (toggle) {
             // Filter the stocks
-            var tempResponse = apiResponse;
             filterStocks(tempResponse);
         }
         else {
             // Else, revert back to original list
+            for (var prop in apiResponse) { apiResponse[prop].filter = false }
             stocks.buildHTML(apiResponse);
         }
     });
